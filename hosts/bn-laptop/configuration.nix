3c514e1bf6f30022036e23433a212a15a70194c5
ham_bn <48649729+hambn@users.noreply.github.com>
2025-12-25 16:52:22 +0000

    .

--- hosts/bn-laptop/configuration.nix
+++ hosts/bn-laptop/configuration.nix
@@ -28,7 +28,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "Asia/Tehran";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
