7b541843dc0a5dc673d63bd96971490d0cd2b8d8
ham_bn <48649729+hambn@users.noreply.github.com>
2025-12-26 23:47:22 +0000

    .

--- modules/programs/virtualisation/bottles.nix
+++ modules/programs/virtualisation/bottles.nix
@@ -4,21 +4,6 @@
     "com.usebottles.bottles"
   ];
 
-  # System packages to support Wine/Vulkan usage
-  environment.systemPackages = with pkgs; [
-    flatpak
-
-    # Wine (32- & 64-bit) helpers
-    wineWowPackages.stable
-    winetricks
-
-    # Vulkan support and translation libs
-    vulkan-loader
-    vulkan-tools
-    dxvk
-    vkd3d
-  ];
-
   # 32-bit OpenGL/Vulkan support required for many Wine prefixes
   hardware.opengl.driSupport32Bit = true;
 }
